0f92195ec668e7c711ae2a86db4c6f0e7c85a21b
Kaide <kaidewu@protonmail.com>
2022-02-13 01:31:46 +0100

    Update todos dados PHP exercise

--- Ejemplos de Examenes/cs-todos-dados/todos-dados-1.php
+++ Ejemplos de Examenes/cs-todos-dados/todos-dados-1.php
@@ -2,10 +2,26 @@
 /**
  * Todos los dados - todos-dados-1.php
  *
- * @author Escriba aquí su nombre
+ * @author Kaide Wu
  */
-
-print "<!-- Ejercicio incompleto -->\n";
+session_name("tododados");
+session_start();
+if (!isset($_SESSION["tiradas"])){
+  $_SESSION["tiradas"] = 0;
+  $_SESSION["dados"] = 0;
+  $_SESSION["dado1"] = 0;
+  $_SESSION["dado2"] = 0;
+  $_SESSION["dado3"] = 0;
+  $_SESSION["dado4"] = 0;
+  $_SESSION["dado5"] = 0;
+  $_SESSION["dado6"] = 0;
+  $_SESSION["alt1"] = "Pendiente";
+  $_SESSION["alt2"] = "Pendiente";
+  $_SESSION["alt3"] = "Pendiente";
+  $_SESSION["alt4"] = "Pendiente";
+  $_SESSION["alt5"] = "Pendiente";
+  $_SESSION["alt6"] = "Pendiente";
+}
 
 ?>
 <!DOCTYPE html>
@@ -14,7 +30,7 @@ print "<!-- Ejercicio incompleto -->\n";
   <meta charset="utf-8">
   <title>
     Todos los dados (Formulario).
-    Escriba aquí su nombre
+    Kaide Wu
   </title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color">
@@ -26,13 +42,34 @@ print "<!-- Ejercicio incompleto -->\n";
   <form action="todos-dados-2.php" method="get">
 <?php
 
-print "    <p class=\"aviso\">Ejercicio incompleto</p>\n";
+if ($_SESSION["tiradas"] != 0){
+  print "    <h2>Tirada de dado</h2>\n";
+  print "\n";
+  print "    <p><img src=\"img/$_SESSION[dados].svg\" alt=\"$_SESSION[dados]\" width=\"140\" height=\"140\"></p>\n";
+  print "\n";
+}
+print "    <h2>Dados ya conseguidos</h2>\n";
+print "\n";
+print "    <p>\n";
+print "      <img src=\"img/$_SESSION[dado1].svg\" alt=\"$_SESSION[alt1]\" width=\"140\" height=\"140\">\n";
+print "      <img src=\"img/$_SESSION[dado2].svg\" alt=\"$_SESSION[alt2]\" width=\"140\" height=\"140\">\n";
+print "      <img src=\"img/$_SESSION[dado3].svg\" alt=\"$_SESSION[alt3]\" width=\"140\" height=\"140\">\n";
+print "      <img src=\"img/$_SESSION[dado4].svg\" alt=\"$_SESSION[alt4]\" width=\"140\" height=\"140\">\n";
+print "      <img src=\"img/$_SESSION[dado5].svg\" alt=\"$_SESSION[alt5]\" width=\"140\" height=\"140\">\n";
+print "      <img src=\"img/$_SESSION[dado6].svg\" alt=\"$_SESSION[alt6]\" width=\"140\" height=\"140\">\n";
+print "    </p>\n";
+print "\n";
+print "    <p>Número de tiradas: $_SESSION[tiradas]</p>\n";
+print "    <p>\n";
+print "      <input type=\"submit\" name=\"siguiente\" value=\"Nueva tirada\">\n";
+print "      <input type=\"submit\" name=\"siguiente\" value=\"Reiniciar\">\n";
+print "    </p>\n";
 
 ?>
   </form>
 
   <footer>
-    <p>Escriba aquí su nombre</p>
+    <p>Kaide Wu</p>
   </footer>
 </body>
 </html>
--- Ejemplos de Examenes/cs-todos-dados/todos-dados-2.php
+++ Ejemplos de Examenes/cs-todos-dados/todos-dados-2.php
@@ -2,7 +2,55 @@
 /**
  * Todos los dados - todos-dados-2.php
  *
- * @author Escriba aquí su nombre
+ * @author Kaide Wu
  */
+session_name("tododados");
+session_start();
+if (!isset($_SESSION["tiradas"])){
+    header("Location: todos-dados-1.php");
+    exit;
+}
 
-print "<p class=\"aviso\">Ejercicio incompleto</p>\n";
+function recoge($var, $m = "")
+{
+      if (!isset($_REQUEST[$var])) {
+          $tmp = (is_array($m)) ? [] : "";
+      } elseif (!is_array($_REQUEST[$var])) {
+          $tmp = trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "UTF-8"));
+      } else {
+          $tmp = $_REQUEST[$var];
+          array_walk_recursive($tmp, function (&$valor) {
+              $valor = trim(htmlspecialchars($valor, ENT_QUOTES, "UTF-8"));
+          });
+      }
+      return $tmp;
+}
+$_SESSION["dados"] = rand(1, 6);
+$siguiente = recoge("siguiente");
+
+if ($siguiente == "Reiniciar"){
+    $_SESSION["tiradas"] = 0;
+    header("Location: todos-dados-1.php");
+}else{
+    $_SESSION["tiradas"] += 1;
+    if ($_SESSION["dados"] == 1){
+        $_SESSION["dado1"] = 1;
+        $_SESSION["alt1"] = 1;
+    }elseif ($_SESSION["dados"] == 2){
+        $_SESSION["dado2"] = 2;
+        $_SESSION["alt2"] = 2;
+    }elseif ($_SESSION["dados"] == 3){
+        $_SESSION["dado3"] = 3;
+        $_SESSION["alt3"] = 3;
+    }elseif ($_SESSION["dados"] == 4){
+        $_SESSION["dado4"] = 4;
+        $_SESSION["alt4"] = 4;
+    }elseif ($_SESSION["dados"] == 5){
+        $_SESSION["dado5"] = 5;
+        $_SESSION["alt5"] = 5;
+    }elseif ($_SESSION["dados"] == 6){
+        $_SESSION["dado6"] = 6;
+        $_SESSION["alt6"] = 6;
+    }
+    header("Location: todos-dados-1.php");
+}
